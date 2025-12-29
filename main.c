61594fd8f33aa02ed0ca14fa3c4413e32ad34575
moriHe <mor.hettich@gmail.com>
2025-12-29 14:54:24 +0100

    Start filtering out labels

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int  main(int argc, char *argv[]) {
 
     // TODO: Make it more dynamic
     char raw_instr[50][50];
-    size_t row = 0;
+    size_t total_rows = 0;
     for (char buf[50]; fgets(buf, sizeof buf, asm_file); asm_file != NULL) {
         if (strlen(buf) > 50) {
             fprintf(stderr, "Error: Max chars per line is 49.\n");
@@ -39,8 +39,21 @@ int  main(int argc, char *argv[]) {
         if (is_empty_line || buf[0] == '\0')
             continue;
         
-        strcpy(raw_instr[row], buf);
-        row++;
+        strcpy(raw_instr[total_rows], buf);
+        total_rows++;
+    }
+
+    for (size_t i = 0; i < total_rows; i++) {
+        if (strchr(raw_instr[i], '(')) {
+            char *close_label = strrchr(raw_instr[i], ')');
+            if (close_label == NULL) {
+                fprintf(stderr, "Error: Row at index %ld does not clsoe the label.", i);
+                return 1;
+            }
+            *close_label = '\0';
+            // TODO: Save in Hash Map. Point to i+1
+            printf("label = %s", raw_instr[i]);
+        }
     }
 
     return 0;
