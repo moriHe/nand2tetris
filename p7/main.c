84d7d584bf6e2ad12f81706d689067090a02d031
moriHe <mor.hettich@gmail.com>
2026-01-31 19:58:15 +0100

    finished all arithmetic implementations with some abstractions. Not tested yet

--- p7/main.c
+++ p7/main.c
@@ -73,79 +73,80 @@ struct Parser {
     int current_index; // needs to start at -1
 };
 
-void decr_sp_load_a(FILE *optr) {
+void load_curr_stack_addr(FILE *optr) {
     fprintf(optr, "@SP\nM=M-1\nA=M\n");
 }
 
-void decr_sp_load_a_m_to_d(FILE *optr) {
-    decr_sp_load_a(optr);
-    fprintf(optr, "D=M\n");
+void calc_latest_two(FILE *optr, char *calc) {
+    load_curr_stack_addr(optr);
+    fprintf(optr, "D=M\n");    
+    load_curr_stack_addr(optr);
+    fptrinf(optr, "%s\n", calc);
+}
+
+void calc_latest_two_push_result_stack(FILE *optr, char *calc) {
+    calc_latest_two(optr, calc);
+    fprintf(optr, "@SP\nA=M\nM=D\n");
+    incr_sp(optr);
 }
 
 void incr_sp(FILE *optr) {
     fprintf(optr, "@SP\nM=M+1\n");
 }
 
-void load_d_in_m(FILE *optr) {
-    fprintf(optr, "@SP\nA=M\nM=D\n");
-}
-
-void load_d_in_m_and_incr_sp(FILE *optr) {
-    load_d_in_m(optr);
-    incr_sp(optr);
-}
-
 void set_m_bool(FILE *optr, int bool_asm_val) {
     fprintf(optr, "@SP\nA=M\nM=%d\n", bool_asm_val);
 }
 
+void calc_bool_op(FILE *optr, int curr_idx, char *op) {
+    calc_latest_two(optr, "D=M-D");
+    fprintf(optr, "@%s_%d\nD;J%s\n", op, curr_idx, op);
+    set_m_bool(optr, 0);
+    fprintf(optr, "@END_%s_%d\n0;JMP\n(%s_%d)\n", op, curr_idx, op, curr_idx);
+    set_m_bool(optr, -1);
+    fprintf(optr, "(END_%s_%d)\n", op, curr_idx);
+    incr_sp(optr);
+}
+
+// Requiremens:
+// * Nand2Tetris is an Empty Stack implementation.
+// * At the end of each operation, RAM[SP] needs to point at en empty value
+// * RAM[SP - 1] is the latest value
 void write_arithmetic(struct Writer *writer, struct Parser *parser) {
     char *instr = parser->current_commands[1];
     int curr_idx = parser->current_index;
     FILE *optr = writer->output_file;
     if (strcmp(instr, "add") == 0) {
-        decr_sp_load_a_m_to_d(optr);
-        decr_sp_load_a(optr);
-        fprintf(optr, "D=D+M\n");
-        load_d_in_m_and_incr_sp(optr);
-    } 
+        calc_latest_two_push_result_stack(optr, "D=D+M");
+    }
     else if (strcmp(instr, "sub") == 0) {
-        decr_sp_load_a_m_to_d(optr);
-        decr_sp_load_a(optr);
-        fprintf(optr, "D=M-D\n");
-        load_d_in_m_and_incr_sp(optr);
+        calc_latest_two_push_result_stack(optr, "D=M-D");
     } 
     else if (strcmp(instr, "neg") == 0) {
-        decr_sp_load_a(optr);
-        fprintf(optr, "M=M-1\n");
+        load_curr_stack_addr(optr);
+        fprintf(optr, "M=-M\n");
         incr_sp(optr);
     }
     else if (strcmp(instr, "eq") == 0) {
-        decr_sp_load_a_m_to_d(optr);
-        decr_sp_load_a(optr);
-        fprintf(optr, "D=M-D\n@EQ_%d\nD;JEQ\n", curr_idx);
-        set_m_bool(optr, 0);
-        fprintf(optr, "@END_EQ_%d\n0;JMP\n(EQ_%d)\n", curr_idx, curr_idx);
-        set_m_bool(optr, -1);
-        fprintf(optr, "(END_EQ_%d)\n", curr_idx);
-        incr_sp(optr);
+        calc_bool_op(optr, curr_idx, "EQ");
     }
     else if (strcmp(instr, "gt") == 0) {
-        decr_sp_load_a_m_to_d(optr);
-        decr_sp_load_a(optr);
-        // maybe can be abstracted with some args? the bit: D=M-D @GT_1 D;JGT
-        // D=M-D is same for all bools. GT and JGT could be handled with args
+        calc_bool_op(optr, curr_idx, "GT");
     }
-    else if (strcmp(instr, "lt") == 0)
-        fprintf();
-    else if (strcmp(instr, "and") == 0)
-        fprintf();
-    else if (strcmp(instr, "or") == 0)
-        fprintf();
-    else if (strcmp(instr, "not") == 0)
-        fprintf();
-        // below is the add statement
-        // Next TODO: Add eq, lt, gt, sub, neg, and, or, not
+    else if (strcmp(instr, "lt") == 0) {
+        calc_bool_op(optr, curr_idx, "LT");
+    }
+    else if (strcmp(instr, "and") == 0) {
+        calc_latest_two_push_result_stack(optr, "D=D&M");
+    }
+    else if (strcmp(instr, "or") == 0) {
+        calc_latest_two_push_result_stack(optr, "D=D|M");
+    }
+    else if (strcmp(instr, "not") == 0) {
+        load_curr_stack_addr(optr);
+        fprintf(optr, "M=!M");
+        incr_sp(optr);
+    }
 }
 
 
