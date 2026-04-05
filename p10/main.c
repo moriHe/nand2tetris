7e227dcac7fd1c30dccc9fb0cb7c95de04931088
moriHe <mor.hettich@gmail.com>
2026-04-05 22:41:49 +0200

    more babystreps

--- p10/main.c
+++ p10/main.c
@@ -3,6 +3,53 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#define HASH_SIZE 101
+
+typedef struct Node {
+    char *key;
+    struct Node *next;
+} Node;
+
+Node *hash_table[HASH_SIZE] = {NULL};
+const char *keywords[] = {"class", "constructor", "method", "field", "static", "var", "int", "char", "boolean", "void", "true", "false", "null", "this", "let", "do", "if", "else", "while", "return", "function"};
+int num_keywords = sizeof(keywords) / sizeof(keywords[0]);
+
+unsigned long hash(const char *str) {
+    unsigned long hash_num = 5381;
+    int c;
+    while (c = *str++) {
+        hash_num = ((hash_num << 5) + hash_num) + c;
+    }
+
+    return hash_num;
+}
+
+void insert_node(const char *key)  {
+    unsigned long hash_num = hash(key);
+    int index = hash_num % HASH_SIZE;
+
+    Node *new_node = malloc(sizeof(Node));
+
+    new_node->key = strdup(key);
+    new_node->next = hash_table[index];
+    hash_table[index] = new_node;
+}
+
+Node *get_node(const char *key) {
+    unsigned long hash_num = hash(key);
+    int index = hash_num % HASH_SIZE;
+
+    Node *node = hash_table[index];
+    while (node != NULL) {
+        if (strcmp(node->key, key) == 0) {
+            return node;
+        }
+        node = node->next;
+    }
+
+    return NULL;
+}
+
 
 bool is_line_overflow(char* buf, FILE *jack_file) {
     if (strchr(buf, '\n') == NULL && !feof(jack_file)) {
@@ -13,42 +60,21 @@ bool is_line_overflow(char* buf, FILE *jack_file) {
 }
 
 void parse_file(FILE *jack_file) {
+    //strchr("{}()[].,;+-*/&|<>=~", c) != NULL;
     char buf[2048];
-    bool is_ml_comment = false;
-    char tmp[2048];
+
     while (fgets(buf, sizeof buf, jack_file) != NULL) {
-        char* current = buf;
-        if (is_line_overflow(current, jack_file)) {
-            fprintf(stderr, "Error: Exceeding line limit of 2048 chars.");
-            break;
-        }
-        if (!is_ml_comment) {
-            char* sl_comment = strstr(current, "//");
-            if (comment != NULL) {
-                *comment = '\0';
-            }
-        }
-
-        for (int i = 0; current[i] != '\0'; i++) {
-            
-            if (is_ml_comment) 
-                continue;
-
-        }
-        char* ml_comment_start = strstr(current, "/*");
-        if (!is_ml_comment && ml_comment_start != NULL) {
-            *ml_comment_start = '\0';
-            is_ml_comment = true;
-        }
-
-        char* ml_comment_end = strstr(current, "*/");
-        if (ml_comment_end != NULL) {
-            is_ml_comment = false;
-        }
+    // TODO: fgets -> strip comments, flag comment
+    // TODO go over each char in fgets and process
     }
+
 }
 
 int main(int argc, char *argv[]) {
+    for (int i = 0; i < num_keywords; i++) {
+        insert_node(keywords[i]);
+    }
+
     // TODO: Implement dir / single file argv. For now assuming it s a jack file
     FILE *jack_file = fopen(argv[1], "r");
     if (jack_file == NULL) {
