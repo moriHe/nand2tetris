c1cc447de49477a6a6ee280147205116d1d9cfe3
moriHe <mor.hettich@gmail.com>
2026-05-07 20:36:53 +0200

    extract parser stuff from main

---
+++ p11/parser.h
@@ -0,0 +1,24 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// TODO: free CurrentInstr when used
+typedef struct CurrentInstr {
+    char *type;
+    char *value;
+} CurrentInstr;
+
+typedef struct Node {
+    char *key;
+    struct Node *next;
+} Node;
+
+CurrentInstr advance_parser(FILE *jack_file);
+Node *get_node(const char *key);
+void insert_node(const char *key);
+unsigned long hash(const char *str);
+bool is_int(const char *str);
+
+#endif
